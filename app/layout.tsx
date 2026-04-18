fd8a9974ffe7945680324b4383d4793b7215cffb
Kevan Kelley <kevan@apexsignsllc.com>
2026-04-18 10:46:57 -0700

    Add Google AdSense script to layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import Script from "next/script";
 import { Barlow, Barlow_Condensed } from "next/font/google";
 import { CookieConsent } from "@/components/CookieConsent";
 import { DisclosureBar } from "@/components/DisclosureBar";
@@ -43,6 +44,14 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <Script
+          async
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9715304698610598"
+          crossOrigin="anonymous"
+          strategy="afterInteractive"
+        />
+      </head>
       <body
         className={`${barlow.variable} ${barlowCondensed.variable} ${barlow.className} min-h-screen bg-brand-black text-brand-bone antialiased`}
       >
